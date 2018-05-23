895c0f539d65d41b88d98bc9ec9d09c1bf6dc8f4
ririron <townageriron@gmail.com>
2018-05-23 10:16:22 +0900

    singleNRZまで

--- No6.py
+++ No6.py
@@ -3,11 +3,15 @@ import random
 #import matplotlib.pyplot as plt
 
 
-#def SingleNRZ():
+def SingleNRZ(a, conv):
+    return conv - a
 
-a = np.array([]);
+a = np.array([])
+conv = np.zeros(1024)
 random.seed(1)
 for i in range(1024):
     a = np.append(a, random.randint(0, 1))
-
 print a
+
+conv = SingleNRZ(a, conv)
+print conv
