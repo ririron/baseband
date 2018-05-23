6ff955e35d6cff207b84147ba4a6a8fb81132004
ririron <townageriron@gmail.com>
2018-05-23 13:52:52 +0900

    グラフ

--- No6.py
+++ No6.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 def SingleNRZ(a, conv):
@@ -9,12 +9,20 @@ def SingleNRZ(a, conv):
 def MultiNRZ(a, conv):
     return conv - 2*a +1
 
+N = 15
+t = np.arange(0, N)
 a = np.array([])
-conv = np.zeros(1024)
+conv = np.zeros(N)
 random.seed(1)
-for i in range(1024):
+for i in range(N):
     a = np.append(a, random.randint(0, 1))
 print a
 
-conv = MultiNRZ(a, conv)
+conv = SingleNRZ(a, conv)
 print conv
+
+F = np.fft.fft(conv)
+F=np.abs(F)**2
+
+plt.plot(t, F)
+plt.show()
