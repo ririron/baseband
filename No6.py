bc9a2ea879ee57a4570039109b55c895f1048552
ririron <townageriron@gmail.com>
2018-05-23 10:23:23 +0900

    singleNRZまで

--- No6.py
+++ No6.py
@@ -6,6 +6,9 @@ import random
 def SingleNRZ(a, conv):
     return conv - a
 
+def MultiNRZ(a, conv):
+    return conv - 2*a +1
+
 a = np.array([])
 conv = np.zeros(1024)
 random.seed(1)
@@ -13,5 +16,5 @@ for i in range(1024):
     a = np.append(a, random.randint(0, 1))
 print a
 
-conv = SingleNRZ(a, conv)
+conv = MultiNRZ(a, conv)
 print conv
